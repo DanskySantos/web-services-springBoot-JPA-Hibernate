47f4e28f5fbd67385c201c4267f239d5dcd2f29e
Dansky Santos <dansky123@hotmail.com>
2022-02-27 18:42:58 -0300

    Category

--- course/src/main/java/com/dansky/course/config/TestConfig.java
+++ course/src/main/java/com/dansky/course/config/TestConfig.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 
 import com.dansky.course.entities.Order;
 import com.dansky.course.entities.enums.OrderStatus;
+import com.dansky.course.repositories.CategoryRepository;
 import com.dansky.course.repositories.OrderRepository;
+import com.dansky.course.entities.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,8 +34,17 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");

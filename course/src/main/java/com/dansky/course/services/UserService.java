3180d5f2b77c6502a9961792489fa269c6bf4484
Dansky Santos <dansky123@hotmail.com>
2022-02-28 21:24:06 -0300

    Exception update

--- course/src/main/java/com/dansky/course/services/UserService.java
+++ course/src/main/java/com/dansky/course/services/UserService.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import com.dansky.course.entities.User;
 import com.dansky.course.repositories.UserRepository;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -43,9 +46,13 @@ public class UserService {
 	}
 
 	public User update (Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {

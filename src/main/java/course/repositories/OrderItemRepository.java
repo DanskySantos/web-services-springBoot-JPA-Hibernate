979677bac21cd078a326eb199894512dd7ccc4d2
Dansky Santos <dansky123@hotmail.com>
2022-03-01 01:41:24 -0300

    fixing project structure

---
+++ src/main/java/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package course.repositories;
+
+import course.entities.OrderItem;
+import course.entities.pk.OrderItemPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+
+}
---
+++ src/main/java/course/resources/UserResource.java
@@ -0,0 +1,67 @@
+package course.resources;
+
+import course.entities.User;
+import course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List <User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
+//	@PostMapping
+//	public User createPerson(@RequestBody User person) {
+//		return service.save(person);
+//	}
+
+//	@PutMapping
+//	public User updatePerson(@RequestBody User person) {
+//		return service.save(person);
+//	}
+//
+//	@DeleteMapping("/{id}")
+//	public String deletePerson (@PathVariable Integer id) {
+//		service.delete(id);
+//		return id + " deletado ";
+//	}
+}

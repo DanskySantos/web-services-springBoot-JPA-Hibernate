dbf693fb06c859a3582180e80bf7ec33d3098e20
Dansky Santos <dansky123@hotmail.com>
2022-02-28 21:09:23 -0300

    Exception delete

--- course/src/main/java/com/dansky/course/services/UserService.java
+++ course/src/main/java/com/dansky/course/services/UserService.java
@@ -3,8 +3,11 @@ package com.dansky.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.dansky.course.services.exceptions.DatabaseException;
 import com.dansky.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.dansky.course.entities.User;
@@ -30,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update (Long id, User obj) {
